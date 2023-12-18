fbe68cdd5fd33d15f0fdc7f404dfb2d14f2e537c
jk <email@jkrause.io>
2023-12-18 12:01:22 +0100

    stats of entries

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,27 @@
-import type { Lang } from "./types";
+import type { ColorsByType, Lang } from "./types";
 
 export const LANGUAGES: Lang[] = ['de', 'en'];
+
+export const COLORS_TEXT: ColorsByType = {
+  action: `text-action dark:text-actionDark`,
+  meetup: `text-meetup`,
+  tech: `text-tech`,
+  cinema: `text-cinema dark:text-cinemaDark`,
+  misc: `text-misc`,
+};
+
+export const COLORS_BG: ColorsByType = {
+  action: `bg-action dark:bg-actionDark`,
+  meetup: `bg-meetup`,
+  tech: `bg-tech`,
+  cinema: `bg-cinema dark:bg-cinemaDark`,
+  misc: `bg-misc`,
+};
+
+export const COLORS_BORDER: ColorsByType = {
+  action: `border-action dark:border-actionDark`,
+  meetup: `border-meetup`,
+  tech: `border-tech`,
+  cinema: `border-cinema dark:border-cinemaDark`,
+  misc: `border-misc`,
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,15 +7,12 @@ export default {
       colors: {
         // Inspired by https://colorhunt.co/palette/2b39643482aa6db3b5f9cc7b
         meetup: '#F7931A', // bitcoin
-        techMeetup: '#0f172a', 
-        techMeetupDark: '#6b7280',
         action: '#2B3964',
         actionDark: '#06b6d4',
-        software: '#3482AA', 
+        tech: '#3482AA', 
         misc: '#6DB3B5', 
         cinema: '#F9CC7B', 
-        cinemaDark: '#e5a32d', 
-        unknown: '#F1E3CB'
+        cinemaDark: '#e5a32d'
       }
     },
 	},
