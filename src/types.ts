05d90454f06c25ad10beb5306e2e787e68690f08
jk <47693+sectore@users.noreply.github.com>
2024-01-08 18:23:25 +0100

    prettier + eslint (#9)
    by following
    
    (1) Get VSCode, eslint & prettier working with Astro https://patheticgeek.dev/blog/astro-prettier-eslint-vscode
    and
    (2) Integrate Prettier with Astro and Tailwind CSS https://straffesites.com/en/blog/integrate-prettier-astro-tailwindcss#adding-tailwind-css-and-prettier

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,15 @@
-import { z } from "astro/zod";
+import { z } from 'astro/zod'
 
-export const eventType = z.enum([
-  "meetup",
-  "action",
-  "tech",
-  "cinema",
-  "misc",
-]);
+export const eventType = z.enum(['meetup', 'action', 'tech', 'cinema', 'misc'])
 
-export type EventType = z.infer<typeof eventType>;
+export type EventType = z.infer<typeof eventType>
 
-const lang = z.enum(["de", "en"]);
-export type Lang = z.infer<typeof lang>;
+const lang = z.enum(['de', 'en'])
+export type Lang = z.infer<typeof lang>
 
-const translations = z.record(
-  lang,
-  z.string()
-)
+const translations = z.record(lang, z.string())
 
-const oTranslations = translations.optional();
+const oTranslations = translations.optional()
 
 const event = z.object({
   type: eventType,
@@ -30,35 +21,30 @@ const event = z.object({
     label: translations,
     url: z.string().url()
   })
-  
-  ,
-});
+})
 
-export type Event = z.infer<typeof event>;
+export type Event = z.infer<typeof event>
 
-export const events = event.array();
+export const events = event.array()
 
-export type Events = z.infer<typeof events>;
+export type Events = z.infer<typeof events>
 
 export const translationKey = z.enum([
-  "title",
-  "blocktime",
+  'title',
+  'blocktime',
   ...eventType.options,
-  "entries",
-  "theEnd",
-  "theBeginning",
-  "builtWith",
-]);
+  'entries',
+  'theEnd',
+  'theBeginning',
+  'builtWith'
+])
 
-export type TranslationKey = z.infer<typeof translationKey>;
+export type TranslationKey = z.infer<typeof translationKey>
 
-export const translation = z.record(
-  translationKey,
-  z.string().min(1)
-  );
+export const translation = z.record(translationKey, z.string().min(1))
 
-export type Translation = z.infer<typeof translation>;
+export type Translation = z.infer<typeof translation>
 
 export type ColorsByType = {
-  [key in EventType]: string;
-};
+  [key in EventType]: string
+}
